6aff871752f7fbdaa91cc164ea02548336b9734b
Nikita <nicandreo@gmail.com>
2022-02-03 12:05:59 +0300

    Initial Commit

---
+++ ContactListSUI/ContactListSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSUIApp.swift
+//  ContactListSUI
+//
+//  Created by Nikita Batrakov on 03.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
